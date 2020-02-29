1b9870126176d00551c3d5e9aa0ff3c3f84c5003
Connor <knelly26@gmail.com>
2020-02-29 12:05:31 -0800

    Put in attribute trags for completeness before we build out the response classes

--- H5MatchEvents/H5MatchEvents.cs
+++ H5MatchEvents/H5MatchEvents.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace HourglassAnalyzer.H5MatchEvents
 {
     public class H5MatchEventSet
     {
+        [JsonPropertyName("IsCompleteSetOfEvents")]
         public bool IsCompleteSetOfEvents {get; set;}
 
     }
